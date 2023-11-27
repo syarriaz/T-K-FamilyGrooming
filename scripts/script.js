bd723d9209b103fe502fc54cd84857f20e96c9c5
syarriaz <152032470+syarriaz@users.noreply.github.com>
2023-11-26 23:03:03 -0500

    Update script.js
    updated hero

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Sandra Arriaza and Ashley Cafiero
+    File Name: script.js
+    Date: 11/25/2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
